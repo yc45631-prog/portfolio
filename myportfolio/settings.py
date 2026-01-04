501f65ec250ba18891dd75b8459dee082e929909
yc45631-prog <yc45631@gmail.com>
2026-01-04 10:20:05 +0300

    Add files via upload

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&w#5__*400i=u0g(d+09kb2mu3%n5jslgw25@u&nf*+b-$hzfu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://www.google.com/url?sa=E&source=gmail&q=yourusername.pythonanywhere.com']
+ALLOWED_HOSTS = ['christianyousef.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -121,7 +121,13 @@ USE_TZ = True
 
 import os
 STATIC_URL = '/static/'
+
+# This is where Django looks for files during development
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# This is where Django PUTS files for production (PythonAnywhere needs this)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
